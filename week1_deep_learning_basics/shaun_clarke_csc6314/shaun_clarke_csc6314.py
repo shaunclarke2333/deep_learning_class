903f0f4ce1d80334906685204c04da39df4b2bdd
Shaun Clarke <shaunclarke43@gmail.com>
2026-03-23 01:13:12 +0000

    started week1 project

--- week1_deep_learning_basics/shaun_clarke_csc6314/shaun_clarke_csc6314.py
+++ week1_deep_learning_basics/shaun_clarke_csc6314/shaun_clarke_csc6314.py
@@ -2,7 +2,16 @@
 Name: Shaun Clarke
 Course: CSC6314 Deep Learning
 Instructor: Margaret Mulhall
-Assignment: 
+Assignment: Project 1
 
+In this project, you will implement and compare a traditional Machine Learning system (Logistic Regression)
+against a Deep Learning system (Multi-Layer Perceptron) using PyTorch. Submit a single .py file.
 
 """
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data
+
+
